a0e7728edec36291832fee0c4609dfe3028cf59d
Kunjal Rajput <kunjalrajput06@gmail.com>
2026-02-15 01:18:29 +0530

    Alternate logic Build

---
+++ logic.js
@@ -0,0 +1,30 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#reset");
+let newgame = document.querySelector("#new-game");
+
+let turn0 = true;
+
+const winPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+     [2, 4, 6],
+     [3, 4, 5],
+     [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click" , () =>{
+        if (turn0 === true){
+            box.innerText = "O";
+            turn0 = false;
+        }
+        else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+    })
+})
