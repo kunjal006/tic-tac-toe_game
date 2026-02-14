50b80564a603a182f39d3ec157521359fe43bc2a
Kunjal Rajput <kunjalrajput06@gmail.com>
2026-02-15 01:39:20 +0530

    Basic logic Build

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset");
 let newgame = document.querySelector("#new-game");
+let msg = document.querySelector("#msg");
+let notify = document.querySelector(".notify");
 
 let turn0 = true;
 
@@ -10,9 +12,9 @@ const winPattern = [
     [0, 4, 8],
     [1, 4, 7],
     [2, 5, 8],
-     [2, 4, 6],
-     [3, 4, 5],
-     [6, 7, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
 ];
 
 boxes.forEach((box) => {
@@ -26,5 +28,35 @@ boxes.forEach((box) => {
             turn0 = true;
         }
         box.disabled = true;
-    })
-})
+        
+        let isWinner = checkWinner();
+    });
+});
+
+
+const checkWinner = () => {
+    for (let pattern of winPattern){
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 != "" && pos2 != "" && pos3 != ""){
+            if(pos1 === pos2 && pos2 === pos3){
+                showWinner(pos1);
+                return true;
+            }
+        }
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText =  `Congratulations, the winner is ${winner}`;
+    notify.classList.remove("hide");
+    disableBoxes();
+};
+
+const disableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = true;
+    }
+};
